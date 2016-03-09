591c64f61fe000cf900dd861bffc7d4f7fc11dfc
Agnes Günther <agnesgunther@Agness-MacBook.local>
2016-03-09 21:46:44 +0100

    background color and logo added

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
+	compass = require('gulp-compass'),
+	minifyCSS = require('gulp-minify-css');
 	livereload = require('gulp-livereload'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat');
  
-gulp.task('sass', function () {
+gulp.task('styles', function () {
   return gulp.src('src/sass/style.sass')
-    .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+  	.pipe(compass({
+      css: 'dist/css',
+      sass: 'src/sass'
+    }))
+    .pipe(minifyCSS())
     .pipe(gulp.dest('dist/css/'))
     .pipe(livereload());
 }); 
@@ -20,8 +25,8 @@ gulp.task('scripts', function() {
 
 gulp.task('watch', function() {
   livereload.listen();
-  gulp.watch('src/sass/**/*.sass', ['sass']);
+  gulp.watch('src/sass/**/*.sass', ['styles']);
   gulp.watch('src/js/**/*.js', ['scripts']);
 });
 
-gulp.task('default', ['sass', 'scripts', 'watch']);
+gulp.task('default', ['styles', 'scripts', 'watch']);
